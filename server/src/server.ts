c935f00118c7ba39b2481be3d50aa1077c85770c
João Alves <joao.alves@ecosis.com.br>
2023-01-17 15:40:51 -0300

    Feat: setup Prisma

---
+++ server/src/server.ts
@@ -0,0 +1,13 @@
+import Fastify from 'fastify'
+
+const app = Fastify()
+
+app.get('/', () => {
+    return 'Hello world'
+})
+
+app.listen({
+    port: 3333,
+}).then(() => {
+    console.log('Server running on port 3333')
+})
